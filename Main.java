258bfea7494a4f79fccc4bca021848c6e2610458
martinjoo <martin@martinjoo.dev>
2024-07-10 22:33:07 +0200

    Clean up

--- Main.java
+++ Main.java
@@ -6,27 +6,24 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
 //        var db = Database.create("example");
-//        var table = db.createTable("products");
+//        var table = db.createTable("users");
 //
         var db = new Database("example");
-//        var table = db.createTable("users");
         var table = db.getTable("users");
-//
-//        table.read();
-//        table.insert(1, "John Doe");
-//        table.insert(2, "Jane Doe");
-//        table.insert(3, "Joe Doe");
-//        table.insert(27, "James Doe");
 
-//        var result = table.selectById(10);
-//
-//        if (result.isPresent()) {
-//            System.out.println(result.get());
-//        } else {
-//            System.out.println("Not found");
-//        }
+        table.insert(1, "John Doe");
+        table.insert(2, "Jane Doe");
+        table.insert(3, "Joe Doe");
 
-        int[] ids = new int[]{119,231};
+        var result = table.selectById(1);
+
+        if (result.isPresent()) {
+            System.out.println(result.get());
+        } else {
+            System.out.println("Not found");
+        }
+
+        int[] ids = new int[]{1,2,3,4,5};
         var results = table.selectByIds(ids);
 
         System.out.println(results);
